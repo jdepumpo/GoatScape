1c622e94b145ae431c15cf9a4b4671e16ba54215
Joe DePumpo <joe@depumpo.com>
2022-11-22 11:25:02 +0000

    fixed conflict

--- app/controllers/goats_controller.rb
+++ app/controllers/goats_controller.rb
@@ -2,7 +2,18 @@ class GoatsController < ApplicationController
   def index
     @goats = Goat.all
   end
-  def show
-    @goat = Goat.find(params[:id])
+
+  def new
+    @goat = Goat.new
+  end
+
+  def create
+    @goat = Goat.new(goat_params)
+
+    if @goat.save
+      redirect_to goat_path(@goat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+# User.create(email: "gerardo.wemans@gmail.com", encrypted_password: "123456")
+Goat.destroy_all
+cat = ["Yoga Goat", "Grazing Goat", "Company Goat", "Birthday Goat", "Decoration Goat", "Seeing Goat", "Guardian Goat", "Entertaining Goat"]
+10.times do |i|
+  goat = Goat.create(name: Faker::Name.first_name, category: cat[rand(0..7)], location: Faker::Address.state, age: rand(1..20), user_id: 1, image_url: "goat_d2m3nl")
+  puts "goat #{i} created"
+  puts goat.name
+  puts goat.user_id
+end

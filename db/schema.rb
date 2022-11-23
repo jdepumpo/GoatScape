4a3a2fe3cde1f658da6e8a3a5d34fa741bd6a4b3
gerardo wemans <gerardo.wemans@gmail.com>
2022-11-23 14:47:11 +0000

    added severa columns to user model, added view for user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_143207) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_115026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_143207) do
     t.integer "transport_fee"
     t.integer "cleaning_fee"
     t.integer "price_per_day"
-    t.float "rating"
+    t.integer "rating"
     t.index ["user_id"], name: "index_goats_on_user_id"
   end
 
@@ -48,6 +48,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_143207) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "phone_number"
+    t.text "bio"
+    t.string "address"
+    t.string "profile_pic"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

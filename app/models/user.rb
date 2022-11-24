9b7cb624c066fca408632f201962ceba2bda1495
Sara Schoonover <saramschoon@gmail.com>
2022-11-24 18:51:33 +0000

    added favorite option

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :goats
   has_many :bookings
+  acts_as_favoritor
 
   # has_one_attached :profile_pic
 end

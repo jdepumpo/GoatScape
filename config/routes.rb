a528028015918cf7cb3140f989cfdb825a22ddb7
Joe DePumpo <joe@depumpo.com>
2022-11-25 12:07:43 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
-  
   resources :goats, only: %i[index show create] do
+    resources :bookings, only: %i[create show]
     member do
       patch :favorite, :unfavorite
     end
   end
-
   get "users/:id/profile", to: "pages#profile", as: :user_profile
 end
